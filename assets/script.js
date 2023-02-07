03f4debbfa95b4372deb8fb597785aeb20eed3f8
ismaouste <ismael.rodmacq@gmail.com>
2023-02-07 17:06:28 +0100

    Implementation of dot event and nextSlide function

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,29 @@ const dots = document.querySelector('.dots')
 slides.forEach((slide, index) => {
 	dots.insertAdjacentHTML('beforeend', `<span class="dot" data-slide="${index}"></span>`)
 })
+
+// when clicking a dot, replace image and tagline
+dots.addEventListener('click', function(e) {
+	if (e.target.classList.contains('dot')) {
+		const slide = e.target.dataset.slide
+		document.querySelector('.banner-img').style.backgroundImage = `url(assets/images/${slides[slide].image})`
+		document.querySelector('#banner p').innerHTML = slides[slide].tagLine
+		// remove active class from all dots
+		dots.querySelectorAll('.dot').forEach(dot => dot.classList.remove('dot_selected'))
+		// add active class to clicked dot
+		e.target.classList.add('dot_selected')
+	}
+})
+function nextSlide() {
+	const img = document.querySelector('.banner-img')
+	const tagline = document.querySelector('#banner p')
+// change image and tagline
+	img.style.backgroundImage = `url(assets/images/${slides[slide].image})`
+	tagline.innerHTML = slides[slide].tagLine
+
+}
+
+
 // give active class to first dot
 dots.querySelector('.dot').classList.add('dot_selected')	
 // insert slides
